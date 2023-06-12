3c8b90a957cc72447ae143bb45eccc8cbb8fd6f5
arulrs-jnpr <arulrs@juniper.net>
2023-06-12 10:52:15 +0530

    Fix login

--- api/db/user.js
+++ api/db/user.js
@@ -3,8 +3,8 @@ const con = require('./conn')
 //CREATE TABLE users (ID SERIAL PRIMARY KEY, name VARCHAR(30),phone bigint,email VARCHAR(30),password VARCHAR(30));
 const createUser = (req, res, next) => {
   var data = req.body
-  username = data['username'], phone = data["phone"], email = data["email"], password = data["password"]
-  con.connPool.query('INSERT INTO users (name, phone, email, password) VALUES ($1, $2, $3, $4)', [username, phone, email, password], (error, results) => {
+  username = data['username'], phone = data["phone"], password = data["password"]
+  con.connPool.query('INSERT INTO users (name, phone, password) VALUES ($1, $2, $3, $4)', [username, phone, password], (error, results) => {
     if(error){
       next(error)
     }else{
